051ea67890d5228ff4d55bdfc72cf12850a4abac
Angus-Liu <angus.liu96@gmail.com>
2019-03-31 09:52:35 +0800

    feat: update mybatis-plus config

---
+++ smilcool-server/src/main/java/com/smilcool/server/base/config/MyBatisConfig.java
@@ -0,0 +1,30 @@
+package com.smilcool.server.base.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MyBatis 配置：
+ * ① 扫描 Mapper
+ * ② 开启事务
+ * ③ 开启分页
+ *
+ * @author Angus
+ * @date 2019/3/20
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.smilcool.server.core.dao")
+public class MyBatisConfig {
+
+    /**
+     * MyBatis Plus 提供的分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
