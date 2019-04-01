646d8d6c9e5a9734515d25279b264ab16a3a7b1e
Angus-Liu <angus.liu96@gmail.com>
2019-04-01 09:29:31 +0800

    chore: close eslint

--- smilcool-admin/.eslintrc.js
+++ smilcool-admin/.eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-parsing-error': [2, {
-      'x-invalid-end-tag': false
-    }],
+    'vue/no-parsing-error': [
+      2,
+      {
+        'x-invalid-end-tag': false
+      }
+    ],
     'no-undef': 'off',
-    'camelcase': 'off',
+    camelcase: 'off',
     // 配置分号结尾，个人习惯
     'semi': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
