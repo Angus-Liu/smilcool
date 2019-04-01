585f916858633757eff7917447e77ead8a6c459d
Angus-Liu <angus.liu96@gmail.com>
2019-04-01 09:29:17 +0800

    feat: update user condition search

--- smilcool-admin/src/view/system/userManage.vue
+++ smilcool-admin/src/view/system/userManage.vue
@@ -15,43 +15,42 @@
           </FormItem>
           <FormItem label="角色" prop="role">
             <Select class="form-item" v-model="userSearchForm.role">
-              <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+              <Option v-for="item in sex" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
           </FormItem>
           <FormItem label="状态" prop="state">
             <Select class="form-item" v-model="userSearchForm.state">
-              <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+              <Option v-for="item in sex" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
           </FormItem>
           <span v-show="more.show">
             <FormItem label="手机" prop="phone">
-              <Input class="form-item" v-model="userSearchForm.phone" placeholder="Enter something..."></Input>
+              <Input class="form-item" v-model="userSearchForm.phone" placeholder="Enter something..."/>
             </FormItem>
             <FormItem label="性别" prop="sex">
               <Select class="form-item" v-model="userSearchForm.sex">
-                <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Option v-for="item in sex" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>
             </FormItem>
             <FormItem label="年级" prop="grade">
-              <DatePicker class="form-item" type="year" v-model="userSearchForm.grade"
-                          placeholder="Select grade"></DatePicker>
+              <DatePicker class="form-item" type="year" :value="userSearchForm.grade" placeholder="Select grade"
+                          @on-change="(value) => userSearchForm.grade = (value !== ''? value : null)"/>
             </FormItem>
             <FormItem label="学院" prop="college">
               <Select class="form-item" v-model="userSearchForm.college">
-                <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Option v-for="item in sex" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>
             </FormItem>
             <FormItem label="专业" props="major">
               <Select class="form-item" v-model="userSearchForm.major">
-                <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Option v-for="item in sex" :value="item.value" :key="item.value">{{ item.label }}</Option>
               </Select>
             </FormItem>
           </span>
           <FormItem>
             <Button class="btn-search" type="primary" icon="ios-search" @click="getUserPage">筛选</Button>
-            <Button @click="handleReset">清空</Button>
-            <a class="drop-down" @click="showMore">
-              {{ more.content }}
+            <Button class="btn-reset" @click="handleReset">清空</Button>
+            <a class="drop-down" @click="showMore">{{ more.content }}
               <Icon :type="more.icon"/>
             </a>
           </FormItem>
@@ -73,7 +72,7 @@
       <!-- 分页 -->
       <Row class="page" justify="end" type="flex">
         <Page :total="page.total" :current="page.current" :page-size="page.size"
-              @on-change="handleCurrentChange" @on-change-size="handlePageSizeChange"
+              @on-change="handleCurrentChange" @on-page-size-change="handlePageSizeChange"
               show-sizer show-total show-elevator/>
       </Row>
     </Card>
@@ -89,14 +88,29 @@ export default {
         content: '展开',
         icon: 'ios-arrow-down'
       },
+      // 查询表单 - 性别
+      sex: [
+        { value: '男', label: '男' },
+        { value: '女', label: '女' },
+        { value: '保密', label: '保密' },
+      ],
+      // 查询表单
+      userSearchForm: {
+        username: null,
+        nickname: null,
+        sex: null,
+        grade: null,
+        college: null,
+        major: null,
+        phone: null,
+        email: null,
+        state: null,
+        role: null,
+        current: 1,
+        size: 10
+      },
       // 表格设置
       loading: true,
-      // 表单数据
-      cityList: [{
-        value: 'New York',
-        label: 'New York'
-      }],
-      model1: '',
       // 表格数据
       columns: [
         { title: '用户名', slot: 'username', width: '150', fixed: 'left' },
@@ -115,22 +129,6 @@ export default {
         { title: '更新时间', key: 'updateTime', align: 'center', width: '150' },
         { title: '操作', slot: 'action', align: 'center', width: '150', fixed: 'right' }
       ],
-      rows: [],
-      // 查询表单
-      userSearchForm: {
-        username: null,
-        nickname: null,
-        sex: null,
-        grade: null,
-        college: null,
-        major: null,
-        phone: null,
-        email: null,
-        state: null,
-        role: null,
-        current: 1,
-        size: 10
-      },
       // 分页对象
       page: {
         records: [],
@@ -161,7 +159,9 @@ export default {
     show (index) {
       this.$Modal.info({
         title: 'User Info',
-        content: `Name：${this.rows[index].name}<br>Age：${this.rows[index].age}<br>Address：${this.rows[index].address}`
+        content: `User Name：${this.page.records[index].username}<br>
+                  Nick Name：${this.page.records[index].nickname}<br>
+                  Sex：${this.page.records[index].sex}`
       });
     },
     getUserPage () {
@@ -172,7 +172,6 @@ export default {
           let result = res.data;
           // 获取分页对象 page
           this.page = result.data;
-
           this.loading = false;
         });
     },
@@ -198,6 +197,7 @@ export default {
       this.getUserPage();
     },
     handlePageSizeChange (size) {
+      console.log(size);
       this.userSearchForm.current = 1;
       this.userSearchForm.size = size;
       this.getUserPage();
@@ -221,6 +221,10 @@ export default {
 
   .btn-search {
     margin-left: -35px;
+    margin-right: 5px;
+  }
+
+  .btn-reset {
     margin-right: 5px;
   }
 </style>
