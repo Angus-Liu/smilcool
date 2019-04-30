50cc1fdc3f0a4e0e9a3f3f835713fe2f00e5fc2a
Angus-Liu <angus.liu96@gmail.com>
2019-04-30 10:03:55 +0800

    feat: add tag table

---
+++ smilcool-server/src/main/java/com/smilcool/server/core/dao/TagMapper.java
@@ -0,0 +1,17 @@
+package com.smilcool.server.core.dao;
+
+import com.smilcool.server.core.pojo.po.Tag;
+
+public interface TagMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Tag record);
+
+    int insertSelective(Tag record);
+
+    Tag selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Tag record);
+
+    int updateByPrimaryKey(Tag record);
+}
