7bb0702fb424086a8f7a61c8d8d6d25437a455f3
Angus-Liu <angus.liu96@gmail.com>
2019-05-01 15:18:38 +0800

    feat: add login action

--- smilcool-client/src/components/common/Header.vue
+++ smilcool-client/src/components/common/Header.vue
@@ -12,7 +12,7 @@
       </nav>
       <div class="avatar">
         <Dropdown @on-click="handleClick">
-          <sui-image class="avatar-img" circular src="http://img.angus-liu.cn/avatar/avatar07.png"/>
+          <sui-image class="avatar-img" circular :src="user.avatar"/>
           <DropdownMenu slot="list">
             <DropdownItem name="login">临时登录</DropdownItem>
             <DropdownItem name="login2">临时登录</DropdownItem>
@@ -27,18 +27,19 @@
       </div>
     </div>
 
-    <!-- 登录注册对话框 -->
-    <Modal v-model="loginRegisterModel.show" :title="loginRegisterModel.title" width="320" footer-hide>
+    <!-- 登录注册模态框 -->
+    <Modal v-model="loginRegisterModel.show" :title="loginRegisterModel.title" :mask-closable="false" width="320"
+           footer-hide>
       <!-- 登录表单 -->
       <sui-form v-if="loginRegisterModel.isLogin">
         <sui-form-field>
-          <sui-input placeholder="请输入用户名或邮箱"/>
+          <sui-input placeholder="请输入用户名或邮箱" v-model="loginForm.username"/>
         </sui-form-field>
         <sui-form-field>
-          <sui-input type="password" placeholder="请输入密码"/>
+          <sui-input type="password" placeholder="请输入密码" v-model="loginForm.password"/>
         </sui-form-field>
         <sui-form-field>
-          <sui-button style="width: 100%" basic primary>登录</sui-button>
+          <sui-button style="width: 100%" basic primary @click.native="login">登录</sui-button>
         </sui-form-field>
         <sui-form-field>
           <div>
@@ -59,7 +60,7 @@
           <sui-input type="password" placeholder="请确认密码"/>
         </sui-form-field>
         <sui-form-field>
-          <sui-button style="width: 100%" basic primary>注册</sui-button>
+          <sui-button style="width: 100%" basic primary @click.native="register">注册</sui-button>
         </sui-form-field>
         <sui-form-field>
           <div style="overflow: hidden">
@@ -67,6 +68,42 @@
           </div>
         </sui-form-field>
       </sui-form>
+
+      <!-- 登录表单 -->
+      <!--<Form ref="loginForm" v-if="loginRegisterModel.isLogin" :model="loginForm" :rules="rules">-->
+      <!--  <FormItem prop="用户名">-->
+      <!--    <Input type="text" v-model="loginForm.username" size="large" placeholder="请输入用户名或邮箱"/>-->
+      <!--  </FormItem>-->
+      <!--  <FormItem prop="密码">-->
+      <!--    <Input type="password" v-model="loginForm.password" size="large" placeholder="请输入密码"/>-->
+      <!--  </FormItem>-->
+      <!--  <FormItem>-->
+      <!--    <Button type="primary" size="large" ghost long @click="login">登录</Button>-->
+      <!--  </FormItem>-->
+      <!--  <div style="font-size: 1rem">-->
+      <!--    <span>没有账号？<a @click="showRegisterForm">注册</a></span>-->
+      <!--    <a style="float:right" href="#">忘记密码</a>-->
+      <!--  </div>-->
+      <!--</Form>-->
+
+      <!-- 注册表单 -->
+      <!--<Form ref="registerForm" v-else :model="registerForm" :rules="rules">-->
+      <!--  <FormItem prop="email">-->
+      <!--    <Input type="text" v-model="registerForm.email" size="large" placeholder="请输入邮箱"/>-->
+      <!--  </FormItem>-->
+      <!--  <FormItem prop="password">-->
+      <!--    <Input type="password" v-model="registerForm.password" size="large" placeholder="请输入密码"/>-->
+      <!--  </FormItem>-->
+      <!--  <FormItem prop="repassword">-->
+      <!--    <Input type="password" v-model="registerForm.password" size="large" placeholder="请确认密码"/>-->
+      <!--  </FormItem>-->
+      <!--  <FormItem>-->
+      <!--    <Button type="primary" size="large" ghost long @click="register">注册</Button>-->
+      <!--  </FormItem>-->
+      <!--  <div style="font-size: 1rem; overflow: hidden">-->
+      <!--    <a style="float:right" @click="showLoginForm">已有账号登录</a>-->
+      <!--  </div>-->
+      <!--</Form>-->
     </Modal>
   </header>
 </template>
@@ -77,11 +114,41 @@ export default {
   data() {
     return {
       input: '',
+      user: {
+        id: -1,
+        username: 'anonymous',
+        nickname: '游客',
+        avatar: require('../../assets/anonymous-avatar.png'),
+        sex: '保密',
+        birthday: '1970-01-01',
+        sign: '一句话介绍自己',
+        intro: '这个人很神秘，什么也没写',
+        grade: '未填写',
+        college: '未填写',
+        major: '未填写',
+        phone: null,
+        email: 'anonymous@example.com',
+        state: 1,
+        remark: null,
+        createTime: '1970-01-01 00:00:00',
+        updateTime: '1970-01-01 00:00:00',
+        roles: ['role_anonymous'],
+        permissions: []
+      },
       loginRegisterModel: {
         show: false,
         title: '登录',
         isLogin: true,
-      }
+      },
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      registerForm: {
+        email: '',
+        password: ''
+      },
+      rules: {}
 
     };
   },
@@ -106,6 +173,28 @@ export default {
     showRegisterForm() {
       this.loginRegisterModel.title = '注册';
       this.loginRegisterModel.isLogin = false;
+    },
+    showErrorNotice(title, desc) {
+      this.$Notice.error({ title, desc });
+    },
+    login() {
+      if (this.loginForm.username.length === 0) {
+        this.showErrorNotice('登录失败', '请填写用户名或邮箱');
+      } else if (this.loginForm.password.length === 0) {
+        this.showErrorNotice('登录失败', '请填写密码');
+      } else {
+        this.$axios.post('/api/user/login', this.loginForm)
+          .then(res => {
+            let result = res.data;
+            if (result.success) {
+              this.loginRegisterModel.show = false;
+              this.user = result.data;
+            }
+          });
+      }
+    },
+    register() {
+      alert('注册');
     }
   },
   mounted() {
@@ -169,6 +258,10 @@ header {
     float: right;
     margin-top: 20px;
     margin-right: 30px;
+  }
+
+  .ivu-form-item:last-child {
+    margin-bottom: 0;
   }
 }
 </style>
