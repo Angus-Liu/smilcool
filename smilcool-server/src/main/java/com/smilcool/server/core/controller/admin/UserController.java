dea11718f0ce7f198e8b1c1e99c026277db69bf9
Angus-Liu <angus.liu96@gmail.com>
2019-05-11 16:59:14 +0800

    chore: update some config

--- smilcool-server/src/main/java/com/smilcool/server/core/controller/admin/UserController.java
+++ smilcool-server/src/main/java/com/smilcool/server/core/controller/admin/UserController.java
@@ -23,48 +23,49 @@ import java.util.List;
 @Slf4j
 @Api(tags = "1.1", description = "用户接口")
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @ApiOperation("用户登录")
-    @PostMapping("/user/login")
+    @PostMapping("/login")
     public Result<UserDetailInfo> login(@RequestBody @Valid UserLoginForm userLoginForm) {
         UserDetailInfo user = userService.login(userLoginForm);
         return Result.success(user);
     }
 
     @ApiOperation("用户注销")
-    @PostMapping("/user/logout")
+    @PostMapping("/logout")
     public Result logout() {
         userService.logout();
         return Result.success();
     }
 
     @ApiOperation("用户注册")
-    @PostMapping("/user/register")
+    @PostMapping("/register")
     public Result<UserDetailInfo> register(@RequestBody @Valid UserRegisterForm userRegisterForm) {
         UserDetailInfo registerUser = userService.register(userRegisterForm);
         return Result.success(registerUser);
     }
 
     @ApiOperation("用户列表")
-    @GetMapping("/user")
+    @GetMapping
     public Result<List<UserDetailInfo>> getUserList() {
         List<UserDetailInfo> userListPage = userService.getUsers();
         return Result.success(userListPage);
     }
 
     @ApiOperation("用户列表（分页条件查询）")
-    @GetMapping("/user/page")
+    @GetMapping("/page")
     public Result<Page<UserDetailInfo>> getUserPage(Page page, UserQueryForm userQueryForm) {
         Page<UserDetailInfo> userListPage = userService.getUsers(page, userQueryForm);
         return Result.success(userListPage);
     }
 
     @ApiOperation(value = "用户信息", notes = "通过用户 id 获取用户信息（包括角色与权限信息）")
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public Result<UserDetailInfo> get(@PathVariable("id") Integer id) {
         UserDetailInfo user = userService.getUserTotalInfo(id);
         return Result.success(user);
--- smilcool-server/src/main/resources/db/init-table.sql
+++ smilcool-server/src/main/resources/db/init-table.sql
@@ -361,15 +361,15 @@ CREATE TABLE `friend` (
 # 消息表（friend）
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
-  `id`             int(11)    NOT NULL AUTO_INCREMENT COMMENT '朋友关联ID',
-  `send_user_id`   int(11)    NOT NULL COMMENT '发送用户ID',
+  `id`              int(11)    NOT NULL AUTO_INCREMENT COMMENT '朋友关联ID',
+  `send_user_id`    int(11)    NOT NULL COMMENT '发送用户ID',
   `receive_user_id` int(11)    NOT NULL COMMENT '接收用户ID',
-  `type`           int(1)     NOT NULL DEFAULT '0' COMMENT '消息类型：0-连接，1-聊天，2-签收，3-心跳，4-拉取好友',
-  `content`        text       NOT NULL COMMENT '内容',
-  `state`          int(1)     NOT NULL DEFAULT '0' COMMENT '状态：0-未签收，1-已签收',
-  `create_time`    datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time`    datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `deleted`        tinyint(1) NOT NULL DEFAULT '0' COMMENT '软删除：0-未删除，1-已删除',
+  `type`            int(1)     NOT NULL DEFAULT '0' COMMENT '消息类型：0-连接，1-聊天，2-签收，3-心跳，4-拉取好友',
+  `content`         text       NOT NULL COMMENT '内容',
+  `state`           int(1)     NOT NULL DEFAULT '0' COMMENT '状态：0-未签收，1-已签收',
+  `create_time`     datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time`     datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted`         tinyint(1) NOT NULL DEFAULT '0' COMMENT '软删除：0-未删除，1-已删除',
   PRIMARY KEY (`id`),
   KEY `idx_send_user_id` (`send_user_id`),
   KEY `idx_receive_user_id` (`receive_user_id`)
