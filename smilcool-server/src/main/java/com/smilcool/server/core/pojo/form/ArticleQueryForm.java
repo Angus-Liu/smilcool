cd53b6ba5245123fb3a0fd963b86204f51cc4f0f
Angus-Liu <angus.liu96@gmail.com>
2019-05-24 16:39:03 +0800

    feat: update article, file & second-hand service

--- smilcool-server/src/main/java/com/smilcool/server/core/pojo/form/ArticleQueryForm.java
+++ smilcool-server/src/main/java/com/smilcool/server/core/pojo/form/ArticleQueryForm.java
@@ -1,9 +1,12 @@
 package com.smilcool.server.core.pojo.form;
 
+import lombok.Data;
+
 /**
  * @author Angus
  * @date 2019/5/21
  */
+@Data
 public class ArticleQueryForm {
-
+    private Integer userId;
 }
--- smilcool-server/src/main/java/com/smilcool/server/core/pojo/form/SecondHandQueryForm.java
+++ smilcool-server/src/main/java/com/smilcool/server/core/pojo/form/SecondHandQueryForm.java
@@ -8,4 +8,5 @@ import lombok.Data;
  */
 @Data
 public class SecondHandQueryForm {
+    private Integer userId;
 }
