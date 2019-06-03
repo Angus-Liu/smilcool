9339965052c1fbfafc3bd5960bed1a64012382af
Angus-Liu <angus.liu96@gmail.com>
2019-06-03 20:07:08 +0800

    test: elasticsearch save method

--- smilcool-server/src/test/java/com/smilcool/server/base/config/elasticsearch/repository/ArticleRepositoryTest.java
+++ smilcool-server/src/test/java/com/smilcool/server/base/config/elasticsearch/repository/ArticleRepositoryTest.java
@@ -46,20 +46,11 @@ public class ArticleRepositoryTest extends SmilcoolServerApplicationTests {
 
     @Test
     public void testSave() {
-        Article article = articleService.getArticle(1);
-//        log.debug("Article: {}", article);
-//        articleRepository.save(articleVO);
-//        Optional<ArticleVO> res = articleRepository.findById(1);
-//        log.debug("ArticleVO From ES: {}", res.get());
-    }
-
-    @Test
-    public void testSave2() {
-        Page<ArticleVO> articlePage = articleService.pageArticleVO(new Page(0, 1000), new ArticleQueryForm());
-        articlePage.getRecords().forEach(articleVO -> {
-            ArticleDocument articleDocument = BeanUtil.copyProp(articleVO, ArticleDocument.class);
+        List<Article> articleList = articleService.listArticle(new ArticleQueryForm());
+        articleList.forEach(article -> {
+            ArticleDocument articleDocument = BeanUtil.copyProp(article, ArticleDocument.class);
             // 将以 JSON 数组形式存储的 tags 字符串字段转化为 List<String> 类型
-            List<String> tags = JSONUtil.parseArray(articleVO.getTags()).toList(String.class);
+            List<String> tags = JSONUtil.parseArray(article.getTags()).toList(String.class);
             articleDocument.setTagList(tags);
             articleRepository.save(articleDocument);
         });
