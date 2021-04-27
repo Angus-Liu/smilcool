8a3b197abba199240fee29bf25514d622d1e2a78
liuanguang <liuanguang@cvte.com>
2021-04-27 19:40:57 +0800

    feat: solve es upgrade

--- smilcool-server/src/main/java/com/smilcool/server/core/service/impl/ArticleServiceImpl.java
+++ smilcool-server/src/main/java/com/smilcool/server/core/service/impl/ArticleServiceImpl.java
@@ -2,7 +2,6 @@ package com.smilcool.server.core.service.impl;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.smilcool.server.base.config.elasticsearch.document.ArticleDocument;
-import com.smilcool.server.base.config.elasticsearch.mapper.HighlightResultMapper;
 import com.smilcool.server.common.enums.DicTypeEnum;
 import com.smilcool.server.common.exception.SmilcoolException;
 import com.smilcool.server.common.util.BeanUtil;
@@ -17,22 +16,11 @@ import com.smilcool.server.core.service.ArticleService;
 import com.smilcool.server.core.service.ResourceService;
 import com.smilcool.server.core.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.Aggregations;
-import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
-import org.elasticsearch.search.aggregations.bucket.terms.StringTerms.Bucket;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -51,9 +39,6 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
 
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -106,42 +91,44 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public List<TagVO> listHotTag() {
-        List<TagVO> hotTagList = new ArrayList<>();
-        // 构建搜索条件
-        SearchQuery searchQuery = new NativeSearchQueryBuilder()
-                // 指定索引
-                .withIndices("article")
-                // 指定类型
-                .withTypes("_doc")
-                // 对 tagList 字段进行聚集搜索，查找热门标签（TOP20）
-                .addAggregation(AggregationBuilders.terms("tagList").field("tagList").size(20))
-                .build();
-        // 执行聚集搜索
-        Aggregations aggregations = elasticsearchTemplate.query(searchQuery, SearchResponse::getAggregations);
-        // 获取热门标签
-        StringTerms terms = (StringTerms) aggregations.getAsMap().get("tagList");
-        List<Bucket> buckets = terms.getBuckets();
-        buckets.forEach(bucket -> hotTagList.add(new TagVO(bucket.getKey().toString(), bucket.getDocCount())));
+//        List<TagVO> hotTagList = new ArrayList<>();
+//        // 构建搜索条件
+//        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+//                // 指定索引
+//                .withIndices("article")
+//                // 指定类型
+//                .withTypes("_doc")
+//                // 对 tagList 字段进行聚集搜索，查找热门标签（TOP20）
+//                .addAggregation(AggregationBuilders.terms("tagList").field("tagList").size(20))
+//                .build();
+//        // 执行聚集搜索
+//        Aggregations aggregations = elasticsearchTemplate.query(searchQuery, SearchResponse::getAggregations);
+//        // 获取热门标签
+//        StringTerms terms = (StringTerms) aggregations.getAsMap().get("tagList");
+//        List<Bucket> buckets = terms.getBuckets();
+//        buckets.forEach(bucket -> hotTagList.add(new TagVO(bucket.getKey().toString(), bucket.getDocCount())));
 
-        return hotTagList;
+//        return hotTagList;
+        return Collections.emptyList();
     }
 
     @Override
     public org.springframework.data.domain.Page<ArticleDocument> search(String q, Integer page, Integer size) {
-        // 创建一个SearchQuery对象
-        SearchQuery searchQuery = new NativeSearchQueryBuilder()
-                // 设置查询条件，此处可以使用 QueryBuilders 创建多种查询
-                .withQuery(QueryBuilders.queryStringQuery(q))
-                // 高亮配置（未匹配时仍然显示 100 字符）
-                .withHighlightBuilder(new HighlightBuilder().noMatchSize(100))
-                // 高亮字段
-                .withHighlightFields(
-                        new HighlightBuilder.Field("title"),
-                        new HighlightBuilder.Field("brief"))
-                // 分页信息
-                .withPageable(PageRequest.of(page, size))
-                // 创建SearchQuery对象
-                .build();
-        return elasticsearchTemplate.queryForPage(searchQuery, ArticleDocument.class, new HighlightResultMapper());
+//        // 创建一个SearchQuery对象
+//        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+//                // 设置查询条件，此处可以使用 QueryBuilders 创建多种查询
+//                .withQuery(QueryBuilders.queryStringQuery(q))
+//                // 高亮配置（未匹配时仍然显示 100 字符）
+//                .withHighlightBuilder(new HighlightBuilder().noMatchSize(100))
+//                // 高亮字段
+//                .withHighlightFields(
+//                        new HighlightBuilder.Field("title"),
+//                        new HighlightBuilder.Field("brief"))
+//                // 分页信息
+//                .withPageable(PageRequest.of(page, size))
+//                // 创建SearchQuery对象
+//                .build();
+//        return elasticsearchTemplate.queryForPage(searchQuery, ArticleDocument.class, new HighlightResultMapper());
+        return null;
     }
 }
